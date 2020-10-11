ddc0fb583f30e8586f914a58a3837fac19d36c98
Andrew <andrew11010700@gmail.com>
2020-10-11 16:23:07 +0300

    Factorial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        final int N = 7;
+        int fact = 1;
+
+        for (int i = 1; i <= N; i++) {
+            fact *= i;
+        }
+
     }
 
 }
